29b5fe41e7b18801f61635d1003dc44b4eafeafd
codysharma-ga <codysharmageneralassembly@gmail.com>
2023-11-18 16:25:11 -0700

    framework part 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { Link, Route, Navigate, Routes } from 'react-router';
 import './App.css';
+import logo from "./logo2.png"
+import masthead from "./masthead.png"
 
 function App() {
+
+  let routes = (
+    <Routes>
+      
+    </Routes>
+  )
+
   return (
     <div className="App">
       <header className="App-header">
+        <div><img id='headerLogo' src={logo} alt="logo"/></div>
+        <div><img id='masthead' src={masthead} alt="masthead"/></div>
       </header>
-      <main>
-        <h1>Welcome to Sharmer news</h1>
-      </main>
+      <body>
+        <nav>
+          {/* {routes} */}
+          <p>Home</p>
+          <p>Politics</p>
+          <p>Sports</p>
+        </nav>
+        <main>
+          <h2>Main display</h2>
+        </main>
+      </body>
     </div>
   );
 }
---
+++ src/todo.md
@@ -0,0 +1,5 @@
+-make components  
+-make routing and links for components  
+-connect to APIs  
+-display items from api on relevant components  
+-format so component shows stories in good format  
