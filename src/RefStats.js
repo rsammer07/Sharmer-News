39d111227d1c16353af13408361c0ab5eebd673c
rsammer07 <ryan.sammer077@gmail.com>
2023-11-29 18:09:32 -0800

    finished prem table and ref stats... started adding current day NFL games

--- src/RefStats.js
+++ src/RefStats.js
@@ -2,32 +2,78 @@ import React, { useEffect, useState } from "react";
 import { Container, Table } from "semantic-ui-react";
 import env from "react-dotenv";
 
-function TopScorers() {
-    const [ref, setRef] = useEffect([]);
+function RefStats() {
+  const [refs, setRefs] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const url = 'https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/table/scorers?how=head';
-            const options = {
-	            method: 'GET',
-	            headers: {
-		            'X-RapidAPI-Key': env.SPORTS_API_URL,
-		            'X-RapidAPI-Host': 'heisenbug-premier-league-live-scores-v1.p.rapidapi.com'
-	            }
-            };
+  useEffect(() => {
+    const fetchData = async () => {
+      const url =
+        "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/table/referee";
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": env.SPORTS_API_URL,
+          "X-RapidAPI-Host": "heisenbug-premier-league-live-scores-v1.p.rapidapi.com",
+        },
+      };
 
-     try {
+      try {
         const response = await fetch(url, options);
         const result = await response.json();
-        setRef(result.records);
+        console.log(result)
+        setRefs(result.referees);
       } catch (error) {
         console.error(error);
-        // Optionally handle the error state, e.g., setTeams([]) or show an error message.
+        // Optionally handle the error state, e.g., setRefs([]) or show an error message.
       }
     };
 
     fetchData();
   }, []);
+
+  console.log(refs);
+
+  return (
+    <div>
+      <h1>Referee Stats for the 23-24 Season</h1>
+      <Container className="ui container refereeContainer" textAlign="center">
+        {refs && refs.length > 0 ? (
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Referee</Table.HeaderCell>
+                <Table.HeaderCell>Games</Table.HeaderCell>
+                <Table.HeaderCell>Home Wins</Table.HeaderCell>
+                <Table.HeaderCell>Draws</Table.HeaderCell>
+                <Table.HeaderCell>Away Wins</Table.HeaderCell>
+                <Table.HeaderCell>Home Goals</Table.HeaderCell>
+                <Table.HeaderCell>Away Goals</Table.HeaderCell>
+                <Table.HeaderCell>Home Red Cards</Table.HeaderCell>
+                <Table.HeaderCell>Away Red Cards</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {refs.map((ref, index) => (
+                <Table.Row key={index}>
+                  <Table.Cell>{ref.referee}</Table.Cell>
+                  <Table.Cell>{ref.games}</Table.Cell>
+                  <Table.Cell>{ref.homewin}</Table.Cell>
+                  <Table.Cell>{ref.draw}</Table.Cell>
+                  <Table.Cell>{ref.awaywin}</Table.Cell>
+                  <Table.Cell>{ref.homegoals}</Table.Cell>
+                  <Table.Cell>{ref.awaygoals}</Table.Cell>
+                  <Table.Cell>{ref.homeredcards}</Table.Cell>
+                  <Table.Cell>{ref.awayredcards}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Container>
+    </div>
+  );
 }
 
-export default TopScorers
+export default RefStats;
